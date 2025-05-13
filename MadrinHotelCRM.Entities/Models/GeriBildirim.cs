efe2595ee270ff651a7201690643982c0b0069c7
zcandemir <150063957+zcandemir@users.noreply.github.com>
2025-05-13 21:15:40 +0300

    Model ve Enum
    Odeme,
    OdemeTipi,
    Personel,
    GeriBildirim tabloları tamamlandır.
    
    Enum:
    OdaTipi,
    OdaDurum,
    OdemeTipi Eklendi

---
+++ MadrinHotelCRM.Entities/Models/GeriBildirim.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MadrinHotelCRM.Entities.Enums;
+
+namespace MadrinHotelCRM.Entities.Models
+{
+    internal class GeriBildirim
+    {
+        public int GeriBildirimId { get; set; }
+        public int MusteriId { get; set; }
+        public Musteri Musteri { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Konu { get; set; }
+        public string Mesaj { get; set; }
+        public bool Durum { get; set; }
+        
+    }
+  
+}
