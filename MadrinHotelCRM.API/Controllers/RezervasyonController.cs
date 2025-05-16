31cdf3b2bc8db35c4bfdd254860b846301c5781f
yaseminozhan <yasemiinozhan@gmail.com>
2025-05-16 16:35:22 +0300

    identity kurulumu yapıldı. controller lar oluşturuldu.

---
+++ MadrinHotelCRM.API/Controllers/RezervasyonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MadrinHotelCRM.API.Controllers
+{
+    public class RezervasyonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MadrinHotelCRM.API/Controllers/TarifeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MadrinHotelCRM.API.Controllers
+{
+    public class TarifeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
