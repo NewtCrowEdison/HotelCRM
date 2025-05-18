7e9b9bafbc7c4d465aaad5a6e89d52c5f6ed7bc8
yaseminozhan <yasemiinozhan@gmail.com>
2025-05-19 01:58:37 +0300

    Oda, OdaTipi, Odeme, Paket servisleri yazıldı

--- MadrinHotelCRM.Services/Services/OdaService.cs
+++ MadrinHotelCRM.Services/Services/OdaService.cs
@@ -1,14 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MadrinHotelCRM.DTO.DTOModels;
+using MadrinHotelCRM.Entities.Enums;
+using MadrinHotelCRM.Entities.Models;
+using MadrinHotelCRM.Repositories.Repositories.Interfaces;
 using MadrinHotelCRM.Services.Interfaces;
 
 namespace MadrinHotelCRM.Services.Services
 {
     public class OdaService : IOdaService
     {
+        private readonly IGenericRepository<Oda> _odaRepo;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public OdaService(
+           IGenericRepository<Oda> odaRepo,
+           IMapper mapper,
+           IUnitOfWork unitOfWork)
+        {
+            _odaRepo = odaRepo;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<OdaDTO> CreateAsync(OdaDTO dto)
+        {
+            var entity = _mapper.Map<Oda>(dto);
+            await _odaRepo.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return _mapper.Map<OdaDTO>(entity);
+        }
 
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _odaRepo.GetByIdAsync(id);
+            if (entity == null) return false;
+            _odaRepo.Delete(entity);
+            await _unitOfWork.CommitAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<OdaDTO>> FindAsync(Expression<Func<Oda, bool>> predicate)
+        {
+            var list = await _odaRepo.FindAsync(predicate);
+            return _mapper.Map<IEnumerable<OdaDTO>>(list);
+        }
+
+        public async Task<IEnumerable<OdaDTO>> GetAllAsync()
+        {
+            var list = await _odaRepo.GetAllAsync();
+            return _mapper.Map<IEnumerable<OdaDTO>>(list);
+        }
+
+        public async Task<OdaDTO> GetByIdAsync(int id)
+        {
+            var entity = await _odaRepo.GetByIdAsync(id);
+            return _mapper.Map<OdaDTO>(entity);
+        }
+
+        public async Task<OdaDTO> UpdateAsync(OdaDTO dto)
+        {
+            var entity = _mapper.Map<Oda>(dto);
+            _odaRepo.Update(entity);
+            await _unitOfWork.CommitAsync();
+            return dto;
+        }
+
+        public async Task<bool> UpdateRoomStatusAsync(int odaId, string yeniDurum)
+        {
+            var oda = await _odaRepo.GetByIdAsync(odaId);
+            if (oda == null) return false;
+
+            if (Enum.TryParse(yeniDurum, out OdaDurum durum))
+            {
+                oda.Durum = durum;
+                _odaRepo.Update(oda);
+                await _unitOfWork.CommitAsync();
+                return true;
+            }
+
+            return false; // Eğer enum dönüştürülemezse false döneriz
+        }
+
+
+        public async Task<bool> UpdateTariffAsync(int odaId, int tarifeId)
+        {
+            var oda = await _odaRepo.GetByIdAsync(odaId);
+            if (oda == null) return false;
+
+            oda.OdaTipiId = tarifeId;
+            _odaRepo.Update(oda);
+            await _unitOfWork.CommitAsync();
+            return true;
+        }
     }
 }
--- MadrinHotelCRM.Services/Services/OdemeService.cs
+++ MadrinHotelCRM.Services/Services/OdemeService.cs
@@ -1,14 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MadrinHotelCRM.DTO.DTOModels;
+using MadrinHotelCRM.Entities.Models;
+using MadrinHotelCRM.Repositories.Repositories.Interfaces;
 using MadrinHotelCRM.Services.Interfaces;
 
 namespace MadrinHotelCRM.Services.Services
 {
     public class OdemeService : IOdemeService
     {
+        private readonly IGenericRepository<Odeme> _odemeRepo;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
+        public OdemeService(
+            IGenericRepository<Odeme> odemeRepo,
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            _odemeRepo = odemeRepo;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<OdemeDTO> CreateAsync(OdemeDTO dto)
+        {
+            var entity = _mapper.Map<Odeme>(dto);
+            await _odemeRepo.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return _mapper.Map<OdemeDTO>(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _odemeRepo.GetByIdAsync(id);
+            if (entity == null) return false;
+
+            _odemeRepo.Delete(entity);
+            await _unitOfWork.CommitAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<OdemeDTO>> FindAsync(Expression<Func<Odeme, bool>> predicate)
+        {
+            var entities = await _odemeRepo.FindAsync(predicate);
+            return _mapper.Map<IEnumerable<OdemeDTO>>(entities);
+        }
+
+        public async Task<IEnumerable<OdemeDTO>> GetAllAsync()
+        {
+            var entities = await _odemeRepo.GetAllAsync();
+            return _mapper.Map<IEnumerable<OdemeDTO>>(entities);
+        }
+
+        public async Task<OdemeDTO> GetByIdAsync(int id)
+        {
+            var entity = await _odemeRepo.GetByIdAsync(id);
+            return _mapper.Map<OdemeDTO>(entity);
+        }
+
+        public async Task<OdemeDTO> UpdateAsync(OdemeDTO dto)
+        {
+            var entity = _mapper.Map<Odeme>(dto);
+            _odemeRepo.Update(entity);
+            await _unitOfWork.CommitAsync();
+            return dto;
+        }
     }
 }
+
