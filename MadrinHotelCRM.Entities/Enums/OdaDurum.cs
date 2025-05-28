2e4aeea261c45a565979fd81a802c70f507537ca
yaseminozhan <yasemiinozhan@gmail.com>
2025-05-29 01:56:09 +0300

    AdminPanel controller oluşturuldu. odaDto ya ekleme yapıldı. oda durum enum una ön planda hoş görünmesi adına  Display name verildi. Admin panelindeki crud sayfaları için Partial viewler oluşturuldu.

--- MadrinHotelCRM.Entities/Enums/OdaDurum.cs
+++ MadrinHotelCRM.Entities/Enums/OdaDurum.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,26 @@ namespace MadrinHotelCRM.Entities.Enums
 {
     public enum OdaDurum
     {
-        Bos=1,
-        Dolu=2,
-        Bakimda=3,
-        Temizlikte=4,
-        Rezervasyonlu=5,
-        Opsionlu=6
+        /// <summary>
+        /// daha da okunabilir hale gelmesi için display attribute ekledik
+        /// </summary>
+        [Display(Name = "Boþ")]
+        Bos = 1,
+
+        [Display(Name = "Dolu")]
+        Dolu = 2,
+
+        [Display(Name = "Bakýmda")]
+        Bakýmda = 3,
+
+        [Display(Name = "Temizlikte")]
+        Temizlikte = 4,
+
+        [Display(Name = "Rezerve Edildi")]
+        Rezervasyonlu = 5,
+
+        [Display(Name = "Opsiyonlu")]
+        Opsionlu = 6
     }
   
 }
