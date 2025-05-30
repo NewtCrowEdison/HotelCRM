3e1ace523fdd30a902100a612d56a108adf6ae2d
yaseminozhan <yasemiinozhan@gmail.com>
2025-05-30 09:35:14 +0300

    bazı değişiklikler yapıldı.

--- MadrinHotelCRMAdmin.MVC/Controllers/AdminPanelController.cs
+++ MadrinHotelCRMAdmin.MVC/Controllers/AdminPanelController.cs
@@ -1,77 +1,52 @@
 ﻿using MadrinHotelCRM.DTO.DTOModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace MadrinHotelCRMAdmin.MVC.Controllers
 {
     public class AdminPanelController : Controller
     {
-        // Sadece Index sayfasını açacak. CRUD işlemleri  API vw AJAX ile yapılcak
-        public IActionResult Index()
+        private readonly HttpClient _api;
+
+       
+        public AdminPanelController(IHttpClientFactory httpFactory)
         {
             _api = httpFactory.CreateClient("ApiClient");
         }
 
+        // Admin panel giriş ekranı
         [HttpGet]
         public IActionResult Index()
         {
-            return View();                      
+            return View();
         }
 
-        //  Oda Yönetimi
-        public IActionResult Odalar() => PartialView("~/Views/_Partials/_OdalarPartial.cshtml");
-        public IActionResult OdaTipleri() => PartialView("~/Views/_Partials/_OdaTipleriPartial.cshtml");
-        public IActionResult OdaTarifeleri() => PartialView("~/Views/_Partials/_OdaTarifeleriPartial.cshtml");
+        ////  Oda CRUD
+        //[HttpPost]
+        //public async Task<IActionResult> OdaEkle(OdaDTO model)
+        //{
+        //    var response = await _api.PostAsJsonAsync("api/oda", model);
+        //    if (response.IsSuccessStatusCode)
+        //        return Ok();
+        //    return BadRequest("Oda eklenirken bir hata oluştu.");
+        //}
 
-        //  Personel Yönetimi
-        public IActionResult Personel() => PartialView("~/Views/_Partials/_PersonelPartial.cshtml");
-        public IActionResult Departmanlar() => PartialView("~/Views/_Partials/_DepartmanlarPartial.cshtml");
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllOdalar()
+        //{
+        //    var data = await _api.GetFromJsonAsync<List<OdaDTO>>("api/oda");
+        //    return PartialView("~/Views/_Partials/_OdalarPartial.cshtml", data);
+        //}
 
-        //  Müşteri ve Etiket
-        public IActionResult Musteriler() => PartialView("~/Views/_Partials/_MusterilerPartial.cshtml");
-        public IActionResult Etiketler() => PartialView("~/Views/_Partials/_EtiketlerPartial.cshtml");
+        //[HttpPost]
+        //public async Task<IActionResult> OdaSil(int id)
+        //{
+        //    var response = await _api.DeleteAsync($"api/oda/{id}");
+        //    if (response.IsSuccessStatusCode)
+        //        return Ok();
 
-        //  Ek Paketler
-        public IActionResult EkPaketler() => PartialView("~/Views/_Partials/_EkPaketlerPartial.cshtml");
-
-        //  Fatura ve Ödeme
-        public IActionResult Faturalar() => PartialView("~/Views/_Partials/_FaturalarPartial.cshtml");
-        public IActionResult Odemeler() => PartialView("~/Views/_Partials/_OdemelerPartial.cshtml");
-        public IActionResult Tarifeler() => PartialView("~/Views/_Partials/_TarifelerPartial.cshtml");
-
-        //  Sistem ve Bildirim
-        public IActionResult SistemLoglari() => PartialView("~/Views/_Partials/_SistemLoglariPartial.cshtml");
-        public IActionResult GeriBildirimler() => PartialView("~/Views/_Partials/_GeriBildirimlerPartial.cshtml");
-        public IActionResult GenelTakip() => PartialView("~/Views/_Partials/_GenelTakipPartial.cshtml");
-
-        //  Oda CRUD 
-
-        [HttpPost]
-        public async Task<IActionResult> OdaEkle(OdaDTO model)
-        {
-            var response = await _api.PostAsJsonAsync("api/oda", model);
-            if (response.IsSuccessStatusCode)
-                return Ok();
-            return BadRequest("Oda eklenirken bir hata oluştu.");
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAllOdalar()
-        {
-            var data = await _api.GetFromJsonAsync<List<OdaDTO>>("api/oda");
-            return PartialView("~/Views/_Partials/_OdalarPartial.cshtml", data);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> OdaSil(int id)
-        {
-            var response = await _api.DeleteAsync($"api/oda/{id}");
-            if (response.IsSuccessStatusCode)
-                return Ok();
-
-            return BadRequest("Oda silinemedi.");
-        }
-
+        //    return BadRequest("Oda silinemedi.");
+        //}
     }
-
 }
-
