dc933e20c4da5f028d4b6a19014a337a1c00fd00
yaseminozhan <yasemiinozhan@gmail.com>
2025-06-12 01:49:25 +0300

    MVCAdmin- admin panel view düzenlemesi ve personel view düzenlemesi yapıldı. MVC: navbardaki rezervasyon butonuna her bir index için (home index, hakkımızda, iletişim) rezervasyon tarih ve seçimi açılır panel eklendi.

--- MadrinHotelCRM.MVC/Controllers/HomeController.cs
+++ MadrinHotelCRM.MVC/Controllers/HomeController.cs
@@ -13,23 +13,16 @@ namespace MadrinHotelCRM.MVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult Hakkimizda()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
+       
+        public IActionResult Hakkimizda() => View();
+        
     }
 }
--- MadrinHotelCRMAdmin.MVC/Controllers/HomeController.cs
+++ MadrinHotelCRMAdmin.MVC/Controllers/HomeController.cs
@@ -13,16 +13,10 @@ namespace MadrinHotelCRMAdmin.MVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        public IActionResult Index() => View();
+      
+        public IActionResult Privacy()=> View();
+      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
